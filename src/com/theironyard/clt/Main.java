97df125d853b0763cd934c2197adb111da540750
Michael Grill <Ultramar@michaels-mbp.home>
2016-05-31 17:25:17 -0400

    work for tomorrow

---
+++ src/com/theironyard/clt/Main.java
@@ -0,0 +1,21 @@
+package com.theironyard.clt;
+
+/**
+ * Created by Ultramar on 5/24/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Double tipCalaulator(Double check, Integer percent) {
+        Double tip = (Double) 0D;
+
+        if (percent.equals((Integer)15)) {
+
+        }
+
+        return tip;
+    }
+}
--- src/com/theironyard/clt/Money.java
+++
@@ -1,7 +0,0 @@
-package com.theironyard.clt;
-
-/**
- * Created by Ultramar on 5/24/16.
- */
-public class Money {
-}
