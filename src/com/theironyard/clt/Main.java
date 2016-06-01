7cbcdcc640e038e0b7c9a747bbda99ef9dc99e23
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-06-01 15:27:56 -0400

    Working tip build

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -1,21 +1,29 @@
 package com.theironyard.clt;
 
+import org.jscience.*;
+import org.jscience.economics.money.Currency;
+import org.jscience.economics.money.Money;
+import org.jscience.physics.amount.Amount;
+
+import javax.measure.quantity.Quantity;
+import javax.measure.unit.Unit;
+import javax.measure.unit.UnitFormat;
+
+import static org.jscience.economics.money.Currency.USD;
+
 /**
  * Created by Ultramar on 5/24/16.
  */
 public class Main {
 
     public static void main(String[] args) {
+        tipCalaulator(20.67, 0.20);
 
     }
 
-    public static Double tipCalaulator(Double check, Integer percent) {
-        Double tip = (Double) 0D;
-
-        if (percent.equals((Integer)15)) {
-
-        }
-
-        return tip;
+    public static Amount<Money> tipCalaulator(double check, double percent) {
+        Amount<Money> start = Amount.valueOf(check, USD);
+        System.out.println(start.times(percent).toText());
+        return start.times(percent);
     }
 }
